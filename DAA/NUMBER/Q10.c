df97a5878f51896e7222df9bfdfd61e5aafe0393
Jahnavi Saxena <149791017+jahnavisaxena@users.noreply.github.com>
2024-09-15 20:49:57 +0530

    Update Q10.c

--- DAA/NUMBER/Q10.c
+++ DAA/NUMBER/Q10.c
@@ -7,7 +7,7 @@ void tenthPattern(int n)
         for(int j=1;j<=i;j++)
         {
             printf("%d ",counter);
-            counter += 2;
+            counter +=2;
         }
         printf("\n");
     }
