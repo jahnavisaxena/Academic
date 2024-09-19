f0950f40e89c4df8620d3c2730b3313472537860
Jahnavi Saxena <149791017+jahnavisaxena@users.noreply.github.com>
2024-09-20 00:42:26 +0530

    Create concat.sh

---
+++ LINUX-SHELL/concat.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+read -p "Enter the first string: " string1
+read -p "Enter the second string: " string2
+concatenated_string="${string1}${string2}"
+echo "Concatenated string: $concatenated_string"
