6b28b3c43c48c206854ae65fe209f8c227b51056
Jahnavi Saxena <149791017+jahnavisaxena@users.noreply.github.com>
2025-01-23 19:03:47 +0530

    Create question3.java

---
+++ Object Oriented Programming/Lab-2/question3.java
@@ -0,0 +1,18 @@
+/*
+ . Fibonacci Series Up to N Terms
+// Import Scanner for user input
+// Prompt the user to enter the number of Fibonacci terms
+// Initialize the first two terms as 0 and 1
+// Print the first two Fibonacci numbers
+// Use a loop to generate the next terms in the sequence
+// Calculate the next number as the sum of the previous two
+// Update variables for the next iteration
+// Print the Fibonacci series
+
+ */
+
+public class question3 {
+    public static void main(String[] args) {
+        
+    }
+}
