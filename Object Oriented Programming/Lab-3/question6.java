f4e9c29bd621ea281d435b0517ef1ac3f26a6fec
Jahnavi Saxena <149791017+jahnavisaxena@users.noreply.github.com>
2025-02-06 22:31:37 +0530

    Create question6.java

---
+++ Object Oriented Programming/Lab-3/question6.java
@@ -0,0 +1,37 @@
+/*
+ A company wants to develop an Employee Management System to track employee details 
+such as name, department, salary, and employee ID. The system should also calculate the 
+total salary expenditure and keep a record of the total number of employees. Implement a 
+Java program by creating an Employee class that includes instance variables for employee 
+ID, name, department, and salary. The class should have a default constructor that 
+initializes employee details with default values and a parameterized constructor that sets 
+employee details based on user input. Use a static variable totalEmployees to track the total 
+number of employees and implement a static method to display this count. Additionally, 
+define a method calculateSalary() that returns the salary of the employee and another 
+method displayEmployeeInfo() to display all employee details. To ensure data 
+encapsulation, mark the salary variable as private and provide a public method to access it. 
+Declare the totalEmployees variable as static so that it is shared among all instances. In the 
+main method, create multiple Employee objects using both default and parameterized 
+constructors. Use the this keyword in the constructors to distinguish between classvariables and 
+constructor parameters. Finally, display the total number of employees and 
+the salary details for each employee. The program should successfully demonstrate the 
+behavior of static and non-static members, the initialization of objects using constructors, 
+and the role of access modifiers in an employee management scenario. 
+ */
+public class question6 {
+    public static void main(String[] args) {
+        
+    }
+}
+class Employee{
+    String name;
+    int ID;
+    String department;
+    double salary;
+
+    Employee(String name,int ID,String department,double salary)
+    {
+        
+    }
+}
+
