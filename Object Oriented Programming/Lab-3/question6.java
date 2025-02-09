abff772ab90e000e39b1d206da16b4ba5646d5a9
Jahnavi Saxena <149791017+jahnavisaxena@users.noreply.github.com>
2025-02-09 19:17:07 +0530

    Update question6.java

--- Object Oriented Programming/Lab-3/question6.java
+++ Object Oriented Programming/Lab-3/question6.java
@@ -20,18 +20,48 @@ and the role of access modifiers in an employee management scenario.
  */
 public class question6 {
     public static void main(String[] args) {
-        
+        Employee e1 = new Employee("John", 1, "HR", 50000);
+        Employee e2 = new Employee("Tanay",2,"IT",60000);
+        Employee e3 = new Employee("Rahul",3,"Finance",70000);
+        Employee e4 = new Employee("Rohan",4,"Marketing",80000);
+        e1.displayEmployeeInfo();
+        e2.displayEmployeeInfo();
+        e3.displayEmployeeInfo();
+        e4.displayEmployeeInfo();
+        Employee.displayTotalEmployess();
     }
 }
 class Employee{
     String name;
     int ID;
     String department;
-    double salary;
-
+    private double salary;
+    static int totalEmployees = 0;
     Employee(String name,int ID,String department,double salary)
     {
-        
+        this.name = name;
+        this.ID = ID;   
+        this.department = department;
+        this.salary = salary;
+
+    }
+    public static void displayTotalEmployess()
+    {
+        System.out.println("Total number of employees are: "+totalEmployees);
+    }
+
+    public double calculateSalary()
+    {
+        return salary;
+    }
+
+    public void displayEmployeeInfo()
+    {
+        totalEmployees++;
+        System.out.println("Name: "+name);
+        System.out.println("ID: "+ID);
+        System.out.println("Department: "+department);
+        System.out.println("Salary: "+calculateSalary());
+        System.out.println("Total number of employees are: "+ totalEmployees);
     }
 }
-
