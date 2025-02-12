d4248948cac9dc51c4babc809af770a3eed7979d
Jahnavi Saxena <149791017+jahnavisaxena@users.noreply.github.com>
2025-02-13 01:00:14 +0530

    Update question1.java

--- Object Oriented Programming/Lab-4/question1.java
+++ Object Oriented Programming/Lab-4/question1.java
@@ -1,1 +1,20 @@
+/*
+ 
+1.	Write a Java program to demonstrate that a private member of a superclass 
+cannot be accessed directly from a derived class. 
 
+ */
+public class question1{
+    public static void main(String[] args) {
+        
+    }
+}
+class Animal{
+    private String name;
+}
+
+class horse extends Animal{
+    void display(){
+        System.out.println(name);
+    }
+}
