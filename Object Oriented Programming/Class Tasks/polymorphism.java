1a34ae1c606bd26f3a93e1afd58eb1acefde39b4
Jahnavi Saxena <149791017+jahnavisaxena@users.noreply.github.com>
2025-02-13 11:19:55 +0530

    Create polymorphism.java

---
+++ Object Oriented Programming/Class Tasks/polymorphism.java
@@ -0,0 +1,37 @@
+//method overloading
+//function with same name but different parameters
+public class polymorphism {
+    public static void main(String[] args) {
+        calculator calc = new calculator();
+        System.out.println(calc.sum(3,5));
+        System.out.println(calc.sum(3,5,78));
+        System.out.println(calc.sum((float)7.3,(float)9.3));//java usually sees decimal as double
+        deer d1 = new deer();
+        d1.eat();//child class wala function hi call hogaa
+    }
+}
+class calculator{
+    int sum(int a ,int b){
+        return a+b;
+    }
+    float sum(float a,float b){
+        return a+b;
+    }
+    int sum(int a,int b,int c){
+        return a+b+c;
+    }
+}
+
+//method overriding:parent and child classes contain same function but with different 
+//definition
+class animal{
+    void eat(){
+        System.out.println("eats anything");
+    }
+}
+
+class deer extends animal{
+    void eat(){
+        System.out.println("eats grass");
+    }
+}
