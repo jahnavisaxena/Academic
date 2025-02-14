ba053bc65a4c633d957190f31f213dbaf5824cf9
Jahnavi Saxena <149791017+jahnavisaxena@users.noreply.github.com>
2025-02-14 12:13:06 +0530

    Create task6.java

---
+++ Object Oriented Programming/Class Tasks/task6.java
@@ -0,0 +1,15 @@
+
+
+public class task6 {
+    public static void main(String[] args) {
+        Cal c  =new Cal();
+        System.out.println("Answer is: "+c.division(4,2));
+    }
+}
+
+class Cal{
+    double division(double a,double b)
+    {
+        return a/b;
+    }
+}
