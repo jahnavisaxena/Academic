9152d45418faf6ed90081fa53a47d64d39d56771
Jahnavi Saxena <149791017+jahnavisaxena@users.noreply.github.com>
2025-02-28 15:55:35 +0530

    Create main.java

---
+++ Object Oriented Programming/Lab-5/main.java
@@ -0,0 +1,10 @@
+import Balance.account;
+
+public class main {
+    public static void main(String[] args) {
+        account ac  = new account();
+        ac.display_balance(1200.00);
+    }
+   
+
+}
