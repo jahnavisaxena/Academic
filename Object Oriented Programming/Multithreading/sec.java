c62ba328cd409388338e74732ae0e77e8a3bbbc7
Jahnavi Saxena <149791017+jahnavisaxena@users.noreply.github.com>
2025-03-17 10:42:28 +0530

    Update sec.java

--- Object Oriented Programming/Multithreading/sec.java
+++ Object Oriented Programming/Multithreading/sec.java
@@ -1,13 +1,36 @@
 public class sec {
     public static void main(String[] args) {
         Employee t1 = new Employee();
+        Manager t2 = new Manager();
+        Employee t3 = new Employee();
+        Manager t4 = new Manager();
         t1.start();
+        System.out.println(Thread.currentThread().getId());
+
+        t2.start();
+        System.out.println(Thread.currentThread().getId());
+
+        t3.start();
+        System.out.println(Thread.currentThread().getId());
+
+        t4.start();
+        System.out.println(Thread.currentThread().getId());
+
+
         System.out.println(Thread.activeCount());
+
     }
 }
 class Employee extends Thread{
     @Override
     public void run() {
         System.out.println("hello");
+    }
+}
+
+class Manager extends Thread{
+    @Override
+    public void run() {
+        System.out.println("manager");
     }
 }
